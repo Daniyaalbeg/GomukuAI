8dfe693c0029c7344120c6343a329dc4170e86b1
daniyaal <danyaalbeg@gmail.com>
2019-02-23 21:09:20 +0000

    Working heuristic

--- Player160983966.java
+++ Player160983966.java
@@ -43,159 +43,162 @@ class Player160983966 extends GomokuPlayer {
                 scoredBoard[i][j] = new squareScore();
             }
         }
-        checkVertical(board, scoredBoard, me);
         checkHorizontal(board, scoredBoard, me);
+        checkVertical(board, scoredBoard, me);
         checkDiagonalL(board, scoredBoard, me);
         checkDiagonalR(board, scoredBoard, me);
+        printScoredBoard(board, scoredBoard);
         return 0;
     }
 
     private void checkHorizontal(Color[][] board, squareScore[][] scoredBoard, Color me) {
-        int whiteCount = 0;
-        int blackCount = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == Color.black) {
-                    //Black Piece
-                    blackCount++;
-                    whiteCount = 0;
-                } else if (board[i][j] == Color.white) {
-                    //White Piece
-                    whiteCount++;
-                    blackCount = 0;
-                } else {
-                    //No Piece
-                    if (blackCount != 0) {
-                        if (blackCount == 4) {
-                            scoredBoard[i][j].blackPoint = (me==Color.black) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                        } else {
-                            scoredBoard[i][j].blackPoint += blackCount;
-                        }
-                        blackCount = 0;
-                    } else {
-                        if (whiteCount == 4) {
-                            scoredBoard[i][j].whitePoint = (me==Color.white) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                        } else {
-                            scoredBoard[i][j].whitePoint += whiteCount;
-                        }
-                        whiteCount = 0;
-                    }
-                }
-            }
-            whiteCount = 0;
-            blackCount = 0;
+        for (int i =0; i < 8; i++) {
+            checkH(board, scoredBoard, me, i, 0, 0, 0, true);
         }
-
         for (int i = 7; i >= 0; i--) {
-            for (int j = 7; j >= 0; j--) {
-                if (board[i][j] == Color.black) {
-                    //Black Piece
-                    blackCount++;
-                    whiteCount = 0;
-                } else if (board[i][j] == Color.white) {
-                    //White Piece
-                    whiteCount++;
-                    blackCount = 0;
-                } else {
-                    //No Piece
-                    if (blackCount != 0) {
-                        if (blackCount == 4) {
-                            scoredBoard[i][j].blackPoint = (me==Color.black) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                        } else {
-                            scoredBoard[i][j].blackPoint += blackCount;
-                        }
-                        blackCount = 0;
-                    } else {
-                        if (whiteCount == 4) {
-                            scoredBoard[i][j].whitePoint = (me==Color.white) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                        } else {
-                            scoredBoard[i][j].whitePoint += whiteCount;
-                        }
-                        whiteCount = 0;
-                    }
-                }
-            }
-            whiteCount = 0;
-            blackCount = 0;
+            checkH(board, scoredBoard, me, i, 7, 0, 0, false);
         }
-        printScoredBoard(scoredBoard);
+    }
+
+    private void checkH(Color[][] board, squareScore[][] scoredBoard, Color me, int row, int col, int whiteCount, int blackCount, boolean right) {
+        if (row < 0 || row > 7 || col < 0 || col > 7) {
+            return;
+        }
+        int add = right ? 1 : -1;
+        if (board[row][col] == null) {
+            if (blackCount == 4) {
+                scoredBoard[row][col].blackPoint = Integer.MAX_VALUE;
+            } else if (whiteCount == 4) {
+                scoredBoard[row][col].whitePoint = Integer.MAX_VALUE;
+            } else {
+                scoredBoard[row][col].blackPoint = (blackCount > scoredBoard[row][col].blackPoint) ? blackCount : scoredBoard[row][col].blackPoint;
+                scoredBoard[row][col].whitePoint = (whiteCount > scoredBoard[row][col].whitePoint) ? whiteCount : scoredBoard[row][col].whitePoint;
+            }
+            checkH(board, scoredBoard, me, row, col+add, 0, 0, right);
+        } else if (board[row][col] == Color.black) {
+            checkH(board, scoredBoard, me, row, col+add, 0, blackCount+1, right);
+        } else {
+            checkH(board, scoredBoard, me, row, col+add, whiteCount+1, 0, right);
+        }
     }
 
     private void checkVertical(Color[][] board, squareScore[][] scoredBoard, Color me) {
-        int whiteCount = 0;
-        int blackCount = 0;
-        //Run down
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[j][i] == Color.black) {
-                    blackCount++;
-                    whiteCount = 0;
-                } else if (board[j][i] == Color.white) {
-                    whiteCount++;
-                    blackCount = 0;
-                } else {
-                    if (blackCount != 0) {
-                        if (blackCount == 4) {
-                            scoredBoard[j][i].blackPoint = (me==Color.black) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                        } else {
-                            scoredBoard[j][i].blackPoint += blackCount;
-                        }
-                        blackCount = 0;
-                    } else {
-                        if (whiteCount == 4) {
-                            scoredBoard[j][i].whitePoint = (me==Color.white) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                        } else {
-                            scoredBoard[j][i].whitePoint += whiteCount;
-                        }
-                        whiteCount = 0;
-                    }
-                }
-            }
-            whiteCount = 0;
-            blackCount = 0;
+        for (int i = 0; i < 8; i++) {
+            checkV(board, scoredBoard, me, 0, i, 0, 0, true);
         }
-        //Run up
         for (int i = 7; i >= 0; i--) {
-            for (int j = 7; j >= 0; j--) {
-                if (board[j][i] == Color.black) {
-                    blackCount++;
-                    whiteCount = 0;
-                } else if (board[j][i] == Color.white) {
-                    whiteCount++;
-                    blackCount = 0;
+            checkV(board, scoredBoard, me, 7, i, 0, 0, false);
+        }
+    }
+
+    private void checkV(Color[][] board, squareScore[][] scoredBoard, Color me, int row, int col, int whiteCount, int blackCount, boolean down) {
+        if (row < 0 || row > 7 || col < 0 || col > 7) {
+            return;
+        }
+        int add = down ? 1 : -1;
+        if (board[row][col] == null) {
+            if (board[row][col] == null) {
+                if (blackCount == 4) {
+                    scoredBoard[row][col].blackPoint = Integer.MAX_VALUE;
+                } else if (whiteCount == 4) {
+                    scoredBoard[row][col].whitePoint = Integer.MAX_VALUE;
                 } else {
-                    if (blackCount != 0) {
-                        if (blackCount == 4) {
-                            scoredBoard[j][i].blackPoint = (me==Color.black) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                        } else {
-                            scoredBoard[j][i].blackPoint += blackCount;
-                        }
-                        blackCount = 0;
-                    } else {
-                        if (whiteCount == 4) {
-                            scoredBoard[j][i].whitePoint = (me==Color.white) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                        } else {
-                            scoredBoard[j][i].whitePoint += whiteCount;
-                        }
-                        whiteCount = 0;
-                    }
+                    scoredBoard[row][col].blackPoint = (blackCount > scoredBoard[row][col].blackPoint) ? blackCount : scoredBoard[row][col].blackPoint;
+                    scoredBoard[row][col].whitePoint = (whiteCount > scoredBoard[row][col].whitePoint) ? whiteCount : scoredBoard[row][col].whitePoint;
                 }
             }
-            whiteCount = 0;
-            blackCount = 0;
+            checkV(board, scoredBoard, me, row+add, col, 0, 0, down);
+        } else if (board[row][col] == Color.black) {
+            checkV(board, scoredBoard, me, row+add, col, 0, blackCount+1, down);
+        } else {
+            checkV(board, scoredBoard, me, row+add, col, whiteCount+1, 0, down);
         }
     }
 
     private void checkDiagonalL(Color[][] board, squareScore[][] scoredBoard, Color me) {
-        for () {
-            for () {
+        //Top to bottom
+        for (int row = 3; row > 0; row--) {
+            checkL(board, scoredBoard, me, row, 0, 0, 0, true);
+        }
+        checkL(board, scoredBoard, me, 0, 0, 0, 0, true);
+        for (int col = 3; col > 0; col--) {
+            checkL(board, scoredBoard, me, 0, col, 0, 0, true);
+        }
 
+        //Bottom to top
+        for (int row = 4; row <= 7 ; row++) {
+            checkL(board, scoredBoard, me, row, 7, 0, 0, false);
+        }
+        checkL(board, scoredBoard, me, 7, 7, 0, 0, false);
+        for (int col = 4; col <= 7; col++) {
+            checkL(board, scoredBoard, me, 7, col, 0, 0, false);
+        }
+    }
+
+    private void checkL(Color[][] board, squareScore[][] scoredBoard, Color me, int row, int col, int whiteCount, int blackCount, boolean up) {
+        if (row > 7 || col > 7 || row < 0 || col < 0) {
+            return;
+        }
+        int add = up ? 1 : -1;
+        if (board[row][col] == null) {
+            if (board[row][col] == null) {
+                if (blackCount == 4) {
+                    scoredBoard[row][col].blackPoint = Integer.MAX_VALUE;
+                } else if (whiteCount == 4) {
+                    scoredBoard[row][col].whitePoint = Integer.MAX_VALUE;
+                } else {
+                    scoredBoard[row][col].blackPoint = (blackCount > scoredBoard[row][col].blackPoint) ? blackCount : scoredBoard[row][col].blackPoint;
+                    scoredBoard[row][col].whitePoint = (whiteCount > scoredBoard[row][col].whitePoint) ? whiteCount : scoredBoard[row][col].whitePoint;
+                }
             }
+            checkL(board, scoredBoard, me, row+add, col+add, 0, 0, up);
+        } else if (board[row][col] == Color.black) {
+            checkL(board, scoredBoard, me, row+add, col+add, 0, blackCount+1, up);
+        } else {
+            checkL(board, scoredBoard, me, row+add, col+add, whiteCount+1, 0, up);
         }
     }
 
     private void checkDiagonalR(Color[][] board, squareScore[][] scoredBoard, Color me) {
+        //Top to bottom
+        for (int col = 4; col < 7; col++) {
+            checkR(board, scoredBoard, me, 0, col, 0, 0, true);
+        }
+        checkR(board, scoredBoard, me, 0, 7, 0, 0, true);
+        for (int row = 1; row < 4; row++) {
+            checkR(board, scoredBoard, me, row, 7, 0, 0, true);
+        }
 
+        //Bottom to top
+        for (int row = 4; row < 7; row++) {
+            checkR(board, scoredBoard, me, row, 0, 0, 0, false);
+        }
+        checkR(board, scoredBoard, me, 7, 0, 0, 0, false);
+        for (int col = 1; col < 4; col++) {
+            checkR(board, scoredBoard, me, 7, col, 0, 0, false);
+        }
+    }
+
+    private void checkR(Color[][] board, squareScore[][] scoredBoard, Color me, int row, int col, int whiteCount, int blackCount, boolean down) {
+        if (row > 7 || col > 7 || row < 0 || col < 0) {
+            return;
+        }
+        int add = down ? 1 : -1;
+        if (board[row][col] == null) {
+            if (blackCount == 4) {
+                scoredBoard[row][col].blackPoint = Integer.MAX_VALUE;
+            } else if (whiteCount == 4) {
+                scoredBoard[row][col].whitePoint = Integer.MAX_VALUE;
+            } else {
+                scoredBoard[row][col].blackPoint = (blackCount > scoredBoard[row][col].blackPoint) ? blackCount : scoredBoard[row][col].blackPoint;
+                scoredBoard[row][col].whitePoint = (whiteCount > scoredBoard[row][col].whitePoint) ? whiteCount : scoredBoard[row][col].whitePoint;
+            }
+            checkR(board, scoredBoard, me, row+add, col-add, 0, 0, down);
+        } else if (board[row][col] == Color.black) {
+            checkR(board, scoredBoard, me, row+add, col-add, 0, blackCount+1, down);
+        } else {
+            checkR(board, scoredBoard, me, row+add, col-add, whiteCount+1, 0, down);
+        }
     }
 
     public void printBoardColor(Color[][] board) {
@@ -215,10 +218,16 @@ class Player160983966 extends GomokuPlayer {
         System.out.println();
     }
 
-    public void printScoredBoard(squareScore[][] board) {
+    public void printScoredBoard(Color[][] board, squareScore[][] scoredBoard) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                System.out.print("|(" + board[i][j].blackPoint + "," + board[i][j].whitePoint + ")|");
+                if (board[i][j] == Color.black) {
+                    System.out.print("|( B )|");
+                } else if (board[i][j] == Color.white) {
+                    System.out.print("|( W )|");
+                } else {
+                    System.out.print("|(" + scoredBoard[i][j].blackPoint + "," + scoredBoard[i][j].whitePoint + ")|");
+                }
             }
             System.out.println("\n");
         }
